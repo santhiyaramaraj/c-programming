22c68d2006a82be90b174019ff67da4fef296cc2
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2017-12-19 16:33:27 +0530

    Update vow or cons.c

--- begginer/vow or cons.c
+++ begginer/vow or cons.c
@@ -2,7 +2,7 @@
 int main()
 {
 	char a;
-	if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+	if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'|a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
 	printf("vowel");
 	else
 	printf("consonant");
