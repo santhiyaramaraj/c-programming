03486112371729c824ff66388fc6aef6beb022f4
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2018-01-25 10:34:35 +0530

    Create elminate odd index&reverse&capitalize the first letter.c

---
+++ tried pograms/elminate odd index&reverse&capitalize the first letter.c
@@ -0,0 +1,37 @@
+//to elminate odd index and store the even index char in a array and reverse the array and capitalize the first letter ina array
+i/p=Winter
+o/p=Rti
+
+************have to do uppercase function in this program ..******incomplete**********
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	// your code goes here
+	char s[1000]="Winter";
+	char s1[1000],r[1000];
+	int i,j=1,n;
+	int l=strlen(s);
+	for(i=0;i<l;i++)        //for eleminating odd numbers
+	{ 
+		if(i%2==0);
+		else
+		{
+			s1[j]=s[i];
+			j++;
+			n=j;
+		}
+		
+	}
+	
+	for(j=1;j<=n;j++)            //for reversing a number
+	{
+	r[j]=s1[n-j];
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%c",r[i]);
+	}
+	
+	
+	return 0;
+}
