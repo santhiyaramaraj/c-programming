6115c29683924b310ab273c4cd426b48afa5fd5f
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2018-03-07 19:45:28 +0530

    Create to find multiple of 13.c

---
+++ begginer/set 8/to find multiple of 13.c
@@ -0,0 +1,20 @@
+// to find multiple of 13
+
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	if(a%13==0)
+	{
+		printf("yes");
+		
+	}
+	else
+	{
+		printf("no");
+	}
+	
+	return 0;
+}
