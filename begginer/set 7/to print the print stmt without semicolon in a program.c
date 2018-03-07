b7831b0c4a5fb34e7a8998a4fd19970d6b39f1c3
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2018-03-07 12:04:01 +0530

    Create to print the print stmt without semicolon in a program.c

---
+++ begginer/set 7/to print the print stmt without semicolon in a program.c
@@ -0,0 +1,16 @@
+// to print the print stmt without semicolon in a program
+
+
+#include <stdio.h>
+int main()
+{
+  char s[10000];
+  scanf("%[^\t\n]s",s);
+  if(printf("%s",s))
+  {
+  	
+  }
+  return 0;	
+}
+  
+  
