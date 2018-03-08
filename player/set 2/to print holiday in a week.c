20a210ebda99a436197e0a718d1bc43b8dc09071
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2018-03-08 19:18:44 +0530

    Create to print holiday in a week.c

---
+++ player/set 2/to print holiday in a week.c
@@ -0,0 +1,21 @@
+to print holiday in a week
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	char s[15];
+	scanf("%s",s);
+	if((s[0]=='s')||(s[0]=='S'))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	
+		
+	return 0;
+}
