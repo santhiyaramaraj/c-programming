daff75e15c581907d23bdf7ba9e429242be9b996
santhiyaramaraj <34671089+santhiyaramaraj@users.noreply.github.com>
2018-03-08 16:44:25 +0530

    Create to print dot at end of string.c

---
+++ player/set1/to print dot at end of string.c
@@ -0,0 +1,12 @@
+to print dot at end of string
+
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	// your code goes here
+	char s1[10000];
+	scanf("%s",s1);
+	printf("%s.",s1);
+	
+	return 0;
+}
